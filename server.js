5f8ef16af3ffebf7b681c8dce62c53c5ddd203f5
Sean Miller <smiller206@me.com>
2018-03-27 09:06:28 -0700

    morning merge 3/27

--- server.js
+++ server.js
@@ -19,7 +19,38 @@ client.on('error', err => console.error(err));
 
 //API Endpoints
 
+// app.get('/new', (request, response) => {};
 app.get('/api/v1/books', (req,res)=> res.send('It lives'));
-//This will need more once then Database is opperational
+//This will need more once the Database is opperational
 app.get('*',(req,res)=> res.redirect(CLIENT_URL));
+// app.post('/api/v1/books', (request, response) => {
+    client.query(``,[],function(err) {if (err) console.error(err);
+    }
+);
+// app.put('/api/v1/books/:id', function(request, response) {client.query(``,[])
+//     .then(() => {client.query(``,[])})
+//     .then(() => {response.send('Update complete');})
+//     .catch(err => {console.error(err);})
+// });
+app.delete('/api/v1/books/:id', (request, response) => {
+    client.query(
+        `DELETE FROM books WHERE book_id=$1;`,
+        [request.params.id]
+    )
+    .then(() => {
+        response.send('Delete complete');
+    })
+    .catch(err => {
+        console.error(err)
+    });
+});
+app.delete('/api/v1/books', (request, response) => {
+    client.query('DELETE FROM books')
+    .then(() => {
+        response.send('Delete complete');
+    })
+    .catch(err => {
+        console.error(err)
+    });
+});
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
